95cf459be4f151bda378428682fe3f149b123fe0
Wojciech Trawinski <wojciech.trawinski@xtb.com>
2023-01-16 14:11:46 +0100

    add user-info service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { ENVIRONMENT } from './environment.token';
 import { LoggerService } from './logger';
 import { init } from './store-tmp';
 import { TARGET } from './target.token';
+import { UserInfoService } from './user-info';
 
 @Component({
   selector: 'app-root',
@@ -20,14 +21,17 @@ export class AppComponent {
   public readonly config = inject(CONFIG, { optional: true });
 
   private readonly configService = inject(ConfigService);
+  private readonly userInfoService = inject(UserInfoService);
   private readonly loggerService = inject(LoggerService);
   private readonly store = inject(Store);
 
   public readonly configState$ = this.configService.state$;
+  public readonly userInfoState$ = this.userInfoService.state$;
 
   constructor() {
     console.log('[AppComponent]: created');
     this.store.dispatch(init({ message: 'Hello from AppComponent' }));
+    this.userInfoService.load().subscribe();
   }
 
   logMessage(): void {
---
+++ src/app/user-info/user-info.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable, tap, timer } from 'rxjs';
+
+import { UserInfo } from './user-info';
+
+interface UserInfoState {
+  isLoaded: boolean;
+  data: UserInfo | null;
+}
+
+const initialState: UserInfoState = {
+  isLoaded: false,
+  data: null,
+};
+
+const mockUserInfo: UserInfo = {
+  name: 'john',
+  isPremium: true,
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserInfoService {
+  private readonly state = new BehaviorSubject<UserInfoState>(initialState);
+
+  public readonly state$ = this.state.asObservable();
+
+  public load(): Observable<UserInfo> {
+    return timer(2000).pipe(
+      map(() => mockUserInfo),
+      tap((userInfo) => {
+        this.state.next({ isLoaded: true, data: userInfo });
+      })
+    );
+  }
+}
